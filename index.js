6d8838fcbbf016824c1f4214b662acc73ecf7af6
Dilshan <dilshan.mudali@gmail.com>
2021-08-30 22:35:11 -0400

    commit tree 63768d86c618cb25d8854d2d3b51eae8f5544ecc

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+function saturdayFun(arg="roller-skate"){
+    return `This Saturday, I want to ${arg}!`
+}
+
+saturdayFun()
+
+const mondayWork = function(arg='go to the office'){
+    return `This Monday, I will ${arg}.`
+}
+
+const wrapAdjective = function(style="*") {
+    return function(adjective="special") {
+      return `You are ${style}${adjective}${style}!`
+    }
+}
